3d494286be461fc80ff278fb7a00a89d377123d3
Renee Yeo <rshu0014@student.monash.edu>
2024-10-10 11:52:04 +0800

    all drafts for graph completed: sankey diagram and lollipop chart

---
+++ lollipopChart.js
@@ -0,0 +1,4 @@
+var lollipopChart = "lollipopChart.json";
+vegaEmbed("#lollipopChart", lollipopChart).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
---
+++ sankeyDiagram.js
@@ -0,0 +1,4 @@
+var sankeyDiagram = "sankeyDiagram.json";
+vegaEmbed("#sankeyDiagram", sankeyDiagram).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
