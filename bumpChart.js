3782855d84bb2adee132de82a894c5121bf375db
Renee Yeo <rshu0014@student.monash.edu>
2024-10-14 01:57:13 +0800

    completion of all graphs

---
+++ bumpChart.js
@@ -0,0 +1,4 @@
+var bumpChart = "bumpChart.json";
+vegaEmbed("#bumpChart", bumpChart).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
